ccc3982fbc8078f52cec1ea629755970496385e3
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-06 17:19:40 +0500

    s

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import brand from '../assets/brand.png';
+import { Link } from 'react-router-dom';
 
 const NavbarDef = () => {
     return (
         <div className='bg-navBg flex justify-between items-center sticky top-0'>
-            <img src={brand} alt="brand" className='w-[300px]' />
+            <Link to="/">
+                <img src={brand} alt="brand" className='w-[300px]' />
+            </Link>
             <p className='font-inika font-bold mr-10 text-xxl text-white'>Javokhir Koziboyev</p>
         </div>
     )
