d72c495d4f3e75b45627c32bb6b420140a85bddb
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-06 18:01:59 +0500

    hammagasalomalaykumbolalar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/index";
 import "./components/global.css";
 import NavbarDef from "./components/navbar/navbar";
-import Table from "./components/user-list/table";
 import UserList from "./components/user-list/userList";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <NavbarDef />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/table" element={<Table />} />
         <Route path="/user info" element={<UserList />} />
       </Routes>
     </>
