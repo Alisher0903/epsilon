ca3fdebcf386789d33a9440eed37b2937e33628b
omadbekolimov <omadbekolimov96@gmail.com>
2024-01-06 15:06:11 +0500

    afff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/index";
 import "./components/global.css";
+import Table from "./components/user-list/table";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/table" element = {<Table/>}/>
       </Routes>
     </>
   );
