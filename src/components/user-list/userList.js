ec5d7983f47f298f3d13aae59fb1f7be2d9c4802
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-06 14:43:57 +0500

    w

--- src/components/user-list/userList.js
+++ src/components/user-list/userList.js
@@ -3,13 +3,7 @@ import React from 'react'
 function UserList() {
   return (
     <div className='ml-3'>
-         <li className="py-4 flex">
-      <img className="h-10 w-10 rounded-full"  alt="" />
-      <div className="ml-3">
-        <p className="text-sm font-medium text-gray-900">qwertyu</p>
-        <p className="text-sm text-gray-500">asdf</p>
-      </div>
-    </li>
+         
     </div>
   )
 }
